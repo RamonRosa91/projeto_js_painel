8a775ac60ddcca05474b3c74f7c5a98b6a8286fb
Ramon Rosa <ramon.costa.rosa@gmail.com>
2022-11-08 20:56:13 -0300

    Melhoria no código
    Foi adicionado erro quando tentam invocar "tocaSom" e melhora na experiencia do usuário ao acessar o site pelo teclado.

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+function tocaSom (seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log ('Elemento não encontrado ou seletor invalido')
+    }
+
+   }
+
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
@@ -12,6 +22,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+
+        if (event.code === 'Space' || event.code === 'Enter') {
+        tecla.classList.add ('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove ('ativa');
     }
     
 }
